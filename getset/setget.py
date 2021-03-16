15d248982f902d80bbb908e2a6669b80c864af32
kamal hasan <hasan.kamaal0072@gmail.com>
2021-03-16 06:01:51 -0800

    accessor and mutator

---
+++ getset/setget.py
@@ -0,0 +1,20 @@
+# class College():
+#     def __init__(self):
+#         self.name="MJP"
+    
+#     def get_name(self):
+#         return self.name
+    
+# x=College()
+# y=x.get_name()
+# print(y)
+
+class Laptop():
+    def __init__(self,roll):
+        self.name=roll
+    
+    def set_name(self):
+        self.name="sony"
+
+x=Laptop(100)
+x.set_name()
